bf10cad731b15a1cc96993132c4e2925dcb2b884
Ernesto Fatuarte Fernandez <ernesto.fatuarte@gmail.com>
2021-05-11 21:55:28 +0200

    Añanidos componentes y plantillas corregidas

--- HolaMundo/src/main/java/com/salesianostriana/dam/holamundo/controller/ControladorMain.java
+++ HolaMundo/src/main/java/com/salesianostriana/dam/holamundo/controller/ControladorMain.java
@@ -12,6 +12,11 @@ public class ControladorMain {
 		return "home";
 	}
 	
+	@GetMapping("/sesion")
+	public String listarAgrupacionesSesionController (Model model) {
+		return "sesion";
+	}
+
 	@GetMapping("/list-sesion")
 	public String listarSesionController (Model model) {
 		return "list-sesion";
@@ -25,5 +30,20 @@ public class ControladorMain {
 	@GetMapping("/list-agrupacion")
 	public String listarAgrupacionesController (Model model) {
 		return "list-agrupacion";
+	}
+
+	@GetMapping("/info-agrupacion")
+	public String mostrarInfoAgrupacionController (Model model) {
+		return "info-agrupacion";
+	}
+	
+	@GetMapping("/clasificacion")
+	public String mostrarPuntosController (Model model) {
+		return "clasificacion";
+	}
+
+	@GetMapping("/radio")
+	public String mostrarRadioController (Model model) {
+		return "radio";
 	}
 }
