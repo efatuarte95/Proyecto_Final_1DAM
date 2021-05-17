310317f70369a931ae0d68c5f18f945df2157197
Ernesto Fatuarte Fernandez <ernesto.fatuarte@gmail.com>
2021-05-17 16:30:56 +0200

    Elimando proyecto holamundo

--- HolaMundo/src/main/java/com/salesianostriana/dam/holamundo/repository/AgrupacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.holamundo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.holamundo.model.Agrupacion;
-
-public interface AgrupacionRepository extends JpaRepository<Agrupacion, Long> {
-
-}
--- HolaMundo/src/main/java/com/salesianostriana/dam/holamundo/repository/SesionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.holamundo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.holamundo.model.Sesion;
-
-public interface SesionRepository extends JpaRepository<Sesion, Long> {
-
-}
